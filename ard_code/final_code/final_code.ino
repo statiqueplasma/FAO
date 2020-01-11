e860fa9729f47fd00ac2b8b24a6917ff6de7bdca
StatiquePlazma <StatiquePlazma@users.noreply.github.com>
2020-01-11 17:00:29 +0100

    new changes

--- ard_code/final_code/final_code.ino
+++ ard_code/final_code/final_code.ino
@@ -1,17 +1,42 @@
 // written for RoboJax.com 
+#include <SoftwareSerial.h>;
 String inputData = ""; // a string to hold incoming data
 boolean check = false; // whether the string is complete
-String inputDatasignature = "$GPGLL";
+String signature = "$GPGLL";
+String ID = "1";
+String LAT;
+String LON;
+int PH;
+int VIS;
+SoftwareSerial Serial4 (10, 11); // RX ; TX
+
 void setup() {
-    // initialize serial:
+    // initialize serial:    
     Serial.begin(9600);
+    Serial2.begin(9600);
+    Serial3.begin(9600);
+    Serial4.begin(9600);
     // reserve 200 bytes for the inputData:
     inputData.reserve(200);
 }
 void loop(){
+  GPSread();
+  delay(1000);
+  dataread(); 
+  delay(1000);
+  Serial1.println(String(ID) + String(LAT) + String(LON) + String(PH) + String(VIS));
   
   
   
+  
+}
+void dataread(){
+  if(Serial4.available()){
+    VIS = Serial4.read();
+  }
+  if(Serial3.available()){
+    PH = Serial3.read();    
+  }
 }
 
 void GPSread() {
@@ -19,22 +44,19 @@ void GPSread() {
     if (check) {
         String BB = inputData.substring(0, 6);
         if (BB == signature) {
-            String LAT = inputData.substring(7, 17);
+            LAT = inputData.substring(7, 17);
             int LATperiod = LAT.indexOf('.');
             int LATzero = LAT.indexOf('0');
             if (LATzero == 0) {
                 LAT = LAT.substring(1);
             }
 
-            String LON = inputData.substring(20, 31);
+            LON = inputData.substring(20, 31);
             int LONperiod = LON.indexOf('.');
             int LONTzero = LON.indexOf('0');
             if (LONTzero == 0) {
                 LON = LON.substring(1);
             }
-
-            Serial.println(LAT);
-            Serial.println(LON);
 
         }
 
--- py_code/map/lineToAdd.py
+++
@@ -1,6 +0,0 @@
-import folium
-import os
-
-folium.Marker([42.376600, -71.129500],
-              popup='<strong>Location One</strong>',
-              tooltip='hello there').add_to('map.html'),
